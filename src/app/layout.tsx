611341096fc60ce60b3a2402e8e47f8c352c0b53
guleDev <gule.dev64@gmail.com>
2024-06-07 19:07:22 -0300

    ajustes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={`${dosis.variable} ${inter.variable} flex items-center flex-col mt-10 bg-neutral-900`}>
+        Ajustar arquivos
         <Image
           src="/images/logo.svg"
           width={200}
